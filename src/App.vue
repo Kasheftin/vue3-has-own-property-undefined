744b2654fb00f2b8252aa35560c2c848a5eeed88
Kasheftin <Kasheftin@gmail.com>
2022-11-28 17:26:24 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <main>
+    <PropTest1 :prop1="1" />
+  </main>
+</template>
+
+<script setup>
+import PropTest1 from './PropTest1.vue'
+</script>
+
---
+++ src/PropTest1.vue
@@ -0,0 +1,22 @@
+<template>
+  <pre>
+    Object.prototype.hasOwnProperty.call(this, 'prop1') = {{ prop1Exists }}
+    this.prop1 = {{ prop1 }}
+  </pre>
+</template>
+
+<script>
+export default {
+  props: {
+    prop1: {
+      type: Number,
+      default: 0
+    }
+  },
+  computed: {
+    prop1Exists() {
+      return Object.prototype.hasOwnProperty.call(this, 'prop1')
+    }
+  }
+}
+</script>
